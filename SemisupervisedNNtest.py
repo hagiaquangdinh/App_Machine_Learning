5722495e6c92f5656b948c992fb7f0caf06fec3f
hagiaquangdinh <21t1020303@husc.edu.vn>
2025-03-14 10:25:38 +0700

    Update SemisupervisedNNtest.py

--- SemisupervisedNNtest.py
+++ SemisupervisedNNtest.py
@@ -335,9 +335,8 @@ def run_PseudoLabellingt_app():
                             with mlflow.start_run():
                                 
                                 cnn = keras.Sequential([
-                                    layers.Input(shape=(X_train.shape[1],)),  # Keep layers.Input
-                                    layers.Reshape((28, 28)),  # Reshape to (28, 28) before Flatten
-                                    layers.Flatten(), 
+                                    layers.Input(shape=(X_train.shape[1],X_train.shape[2])),  # Keep layers.Input
+                                     
                                     *[layers.Dense(num_neurons, activation=activation) for _ in range(num_layers)],
                                     layers.Dense(10, activation="softmax")
                                 ])
